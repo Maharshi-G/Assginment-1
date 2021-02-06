d56de64de5197ba8bbe514a237951e020ea52b51
Maharshi <maharshi13@hotmail.com>
2021-02-06 00:36:09 -0500

    Added boxes, box boundary still broke

--- MCM_Electron_Transport_With_Scattering_And_Boxes.m
+++ MCM_Electron_Transport_With_Scattering_And_Boxes.m
@@ -14,8 +14,8 @@ V_thermal = sqrt(2*k*T/Mass_n); %Thermal Veleocity
 Height = 100e-9; % The height of the simulation environment
 Length = 200e-9; % The lengthof the simulation environment
 nElectrons = 2e3; % Total number of electrons to simulate
-nPlotted_Electrons = 10; %Total number of electrons displayed 
-Time_Step = Height/V_thermal/100; % Time step of simulation
+nPlotted_Electrons = 50; %Total number of electrons displayed 
+Time_Step = Height/V_thermal/150; % Time step of simulation
 Iterations = 1000; % Number of iternations to simulate
 Show_Movie = 1; %Display steps control
 % The mean free path is determined by multipling the thermal velocity 
@@ -27,42 +27,65 @@ Electron_State = zeros(nElectrons,4);
 %Trajectories will be recorded in the array below, where there are 
 ... double the colums as we need both the x and y positions for each
     ... of the "to-be-plotted" electrons
-Trajectories = zeros(Iterations,nPlotted_Electrons*2);
+Trajectories = zeros(Iterations,nPlotted_Electrons);
 %Temperature will be recorded in the array below
 Temperature = zeros(Iterations,1);
 %Create a scattering probability
-P_Scatterieng = 1 - exp(-Time_Step/0.2e-12);
+P_Scatterieng =0; %1 - exp(-Time_Step/0.2e-12);
 %Create a distribution using the matlab makedist function
 Velocity_PDF = makedist('Normal', 'mu', 0, 'sigma', sqrt(k*T/Mass_n));
-
-
 
 %Setting the top/bottom of the boxes specularity
 Box_Top_Specular = 1;
 Box_Bottom_Specular = 1;
 %Create Box-positions [x1, x2, y1,y2]
-Box_pos = 1e-9*[80 120 0 40; 80 120 60 100];
-%Create the state of the box (specular or 
+Box_pos = 1e-9.*[80 120 0 40; 80 120 60 100];
+%Create the state of the box (specular or diffusive)
 Box_state = [0 1];
 
 
 %Generate a random inital population postion and velocity
 for i = 1:nElectrons
    Electron_State(i,:) = [Length*rand() Height*rand() random(Velocity_PDF) random(Velocity_PDF)];
-   
-   
-%    if ( ((80e-9<=Electron_State(i,1))&&(Electron_State(i,1)<=120e-9)) && ...
-%        ((0<=Electron_State(i,2))&&(Electron_State(i,2)<=40e-9)) )
-%        Electron_State(i:1) = Length*rand();
-%        Electron_State(i:2) = Height*rand();
-%    elseif ( ((80e-9<=Electron_State(i,1))&&(Electron_State(i,1)<=120e-9)) && ...
-%        ((50e-9<=Electron_State(i,2))&&(Electron_State(i,2)<=Height)) )
-%        Electron_State(i:1) = Length*rand();
-%        Electron_State(i:2) = Height*rand();
-%    end
+%    if ( (Electron_State(i,1)>80e-9 && Electron_State(i,1)<120e-9) && (Electron_State(i,2)>0 && Electron_State(i,2)<40e-9))
+%        Electron_State(i,1) = Length*rand();
+%        Electron_State(i,2) = Height*rand();
+%    elseif ( (Electron_State(i,1)>80e-9 && Electron_State(i,1)<120e-9) && (Electron_State(i,2)>60e-9 && Electron_State(i,2)<Height))
+%        Electron_State(i,1) = Length*rand();
+%        Electron_State(i,2) = Height*rand();
+%    end     
+   Electron_State( (Electron_State(:,1)>80e-9 & Electron_State(:,1)<120e-9),1) = Length*rand();
+   Electron_State( (((Electron_State(:,2)>0 & Electron_State(:,2)<40e-9) | (Electron_State(:,2)>60e-9...
+       & Electron_State(:,2)<100e-9)) & (Electron_State(:,1)>80e-9 & Electron_State(:,1)<120e-9)),2) = Height*rand();
+%    plot(Electron_State(1:nPlotted_Electrons,1)./1e-9,Electron_State(1:nPlotted_Electrons,2)./1e-9,'o');
+%    grid on;
+%    axis([0 Length/1e-9 0 Height/1e-9]);
+%    hold on;
+%     for j=1:size(Box_pos,1)
+%        plot([Box_pos(j, 1) Box_pos(j, 1) Box_pos(j, 2) Box_pos(j, 2) Box_pos(j, 1)]./1e-9,...
+%            [Box_pos(j, 3) Box_pos(j, 4) Box_pos(j, 4) Box_pos(j, 3) Box_pos(j, 3)]./1e-9, 'k-');
+%     end
+%     hold off;
 end
 
-%Average velocity calculation
+figure(1)
+plot(Electron_State(1:nPlotted_Electrons,1)./1e-9,Electron_State(1:nPlotted_Electrons,2)./1e-9,'o');
+grid on;
+axis([0 Length/1e-9 0 Height/1e-9]);
+xlabel('x (nm)');
+ylabel('y (nm)');
+title(sprintf("Plotting (%d/%d) electron at constant velocity",nPlotted_Electrons,nElectrons));
+hold on;
+for j=1:size(Box_pos,1)
+   plot([Box_pos(j, 1) Box_pos(j, 1) Box_pos(j, 2) Box_pos(j, 2) Box_pos(j, 1)]./1e-9,...
+       [Box_pos(j, 3) Box_pos(j, 4) Box_pos(j, 4) Box_pos(j, 3) Box_pos(j, 3)]./1e-9, 'k-');
+end
+hold off;
+% Electron_State((Electron_State(:,1)>40e-9 & Electron_State(:,1)<120e-9 ),1) = Electron_State((Electron_State(:,1)>40e-9 & Electron_State(:,1)<120e-9 ),1) * (Length*rand());
+% Electron_State((Electron_State(:,2)>0 & Electron_State(:,2)<40e-9),2) = Electron_State((Electron_State(:,2)>0 & Electron_State(:,2)<40e-9),2) * (Height*rand());
+% Electron_State((Electron_State(:,2)>50e-9 & Electron_State(:,2)<Height),2) = Electron_State((Electron_State(:,2)>50e-9 & Electron_State(:,2)<Height),2) * (Height*rand());
+% plot(Electron_State(1:nPlotted_Electrons,1)./1e-9,Electron_State(1:nPlotted_Electrons,2)./1e-9,'o');
+
 Average_Velocity = sqrt ((Electron_State(:,3).^2)/nElectrons + (Electron_State(:,4).^2)/nElectrons);
 
 %We will now move (iterate) over time, updating the positions and direction
@@ -80,20 +103,42 @@ for i = 1:Iterations
     %Check if and move all electrons at X=0nm Bound:
     Electron_State((Electron_State(:,1)<0),1) =Electron_State((Electron_State(:,1)<0),1) + Length;
     
-    %Check if and move all electrons at Y=100nm Bound:
+    %Check if and move all electrons at Y Bounds and if specular or diffusive:
     if (Box_Top_Specular == 1)
        Electron_State((Electron_State(:,2)>Height),4) = -1*Electron_State((Electron_State(:,2)>Height),4) ;
-    elseif (Box_Top_Specular == 0)
+    else
        Electron_State((Electron_State(:,2)>Height),2) = Electron_State((Electron_State(:,2)>Height),2) - Height ;
     end
     if (Box_Bottom_Specular == 1)
         Electron_State((Electron_State(:,2)<0),4) =  -1*Electron_State((Electron_State(:,2)<0),4) ;
-    elseif (Box_Bottom_Specular == 0)
+    else
         Electron_State((Electron_State(:,2)<0),2) =  Electron_State((Electron_State(:,2)<0),2) + Height;
     end
     
+    %Check if making contact with Bottom Box going Positive Vx
+    Electron_State( (Electron_State(:,1)<=120e-9 & Electron_State(:,1)>=80e-9 & Electron_State(:,2)<40e-9 & Electron_State(:,3)>0) ,3) =...
+       -Electron_State( (Electron_State(:,1)<120e-9 & Electron_State(:,1)>80e-9 & Electron_State(:,2)<40e-9 & Electron_State(:,3)>0) ,3);
+    %Check if making contact with Bottom Box going Negative Vx
+   Electron_State( (Electron_State(:,1)<=120e-9 & Electron_State(:,1)>=80e-9 & Electron_State(:,2)<40e-9 & Electron_State(:,3)<0) ,3) =...
+       -Electron_State( (Electron_State(:,1)<120e-9 & Electron_State(:,1)>80e-9 & Electron_State(:,2)<40e-9 & Electron_State(:,3)<0) ,3);
+   
+   %Check if making contact with Top Box going Postive Vx
+   Electron_State( (Electron_State(:,1)<=120e-9 & Electron_State(:,1)>=80e-9 & Electron_State(:,2)>60e-9 & Electron_State(:,2)<100e-9...
+       & Electron_State(:,3)>0) ,3) = -Electron_State( (Electron_State(:,1)<=120e-9 & Electron_State(:,1)>=80e-9 & ...
+       Electron_State(:,2)>60e-9 & Electron_State(:,2)<100e-9 & Electron_State(:,3)>0) ,3);
+   %Check if making contact with Top Box going Negative Vx
+   Electron_State( (Electron_State(:,1)<=120e-9 & Electron_State(:,1)>=80e-9 & Electron_State(:,2)>60e-9 & Electron_State(:,2)<100e-9...
+       & Electron_State(:,3)<0) ,3) = -Electron_State( (Electron_State(:,1)<=120e-9 & Electron_State(:,1)>=80e-9 & ...
+       Electron_State(:,2)>60e-9 & Electron_State(:,2)<100e-9 & Electron_State(:,3)<0) ,3);
+   
+   
+   
+%     Electron_State( (Electron_State(:,1)>0 & Electron_State(:,1)<=80e-9 & Electron_State(:,2)<40e-9 & Electron_State(:,3)>0) ,3:4) =...
+%        -1*Electron_State( (Electron_State(:,1)>0 & Electron_State(:,1)<=80e-9 & Electron_State(:,2)<40e-9 & Electron_State(:,3)>0) ,3:4);
+%     
+%     Electron_State( (Electron_State(:,1)>0 & Electron_State(:,1)<=80e-9 & Electron_State(:,2)>60e-9 & Electron_State(:,3)>0) ,3) =...
+%         -1*Electron_State( (Electron_State(:,1)>0 & Electron_State(:,1)<=80e-9 & Electron_State(:,2)>60e-9 & Electron_State(:,3)>0) ,3);
 
-    
     %Add scattering
      j = rand(nElectrons,1) < P_Scatterieng;
      Electron_State(j,3:4) = random(Velocity_PDF,[sum(j),2]);
@@ -108,9 +153,8 @@ for i = 1:Iterations
     Temperature(i) = ( sum (Electron_State(:,3).^2) + sum(Electron_State(:,4).^2)) * Mass_n / k / 2 / nElectrons;
     
     %Shows the pathing of the electron, as well as the updating trajectory
-    if Show_Movie && mod(i,50)
+    if Show_Movie && mod(i,20)
        figure(1)
-       hold off;
        plot(Electron_State(1:nPlotted_Electrons,1)./1e-9,Electron_State(1:nPlotted_Electrons,2)./1e-9,'o');
        grid on;
        axis([0 Length/1e-9 0 Height/1e-9]);
@@ -118,31 +162,43 @@ for i = 1:Iterations
        ylabel('y (nm)');
        title(sprintf("Plotting (%d/%d) electron at constant velocity",nPlotted_Electrons,nElectrons));
        hold on;
+       for j=1:size(Box_pos,1)
+           plot([Box_pos(j, 1) Box_pos(j, 1) Box_pos(j, 2) Box_pos(j, 2) Box_pos(j, 1)]./1e-9,...
+               [Box_pos(j, 3) Box_pos(j, 4) Box_pos(j, 4) Box_pos(j, 3) Box_pos(j, 3)]./1e-9, 'k-');
+       end
+       hold off;
     end
 end
 figure("name","Trajectory, temperature and speed results results")
 subplot(3,1,1)
-hold on;
 for i = 1:nPlotted_Electrons
-    plot(Trajectories(:,i)./1e-9, Trajectories(:,i+1)./1e-9,'-');
+    plot(Trajectories(:,i)./1e-9, Trajectories(:,i+1)./1e-9,'.');
 end
+hold on;
+for j=1:size(Box_pos,1)
+   plot([Box_pos(j, 1) Box_pos(j, 1) Box_pos(j, 2) Box_pos(j, 2) Box_pos(j, 1)]./1e-9,...
+       [Box_pos(j, 3) Box_pos(j, 4) Box_pos(j, 4) Box_pos(j, 3) Box_pos(j, 3)]./1e-9, 'k-');
+end
+hold off;
 axis([0 Length/1e-9 0 Height/1e-9]);
 xlabel('x (nm)');
 ylabel('y (nm)');
 grid on;
 title(sprintf("Trajectories of (%d/%d) electron(s) at constant velocity",nPlotted_Electrons,nElectrons));
 
+
 subplot(3,1,2)
 plot(Time_Step*(0:Iterations-1), Temperature);
 grid on;
-title('Temperature');
+xlim([0 Time_Step*Iterations])
+title('Temperature of the region');
 xlabel('Time (s)');
 ylabel('Temperature (K)');
 
 subplot(3,1,3)
 Velocity = sqrt(Electron_State(:,3).^2 + Electron_State(:,4).^2);
-title("Electron Speed");
 histogram(Velocity);
+title('Electron Speed');
 xlabel("Speed (m/s)");
 ylabel("Number of particles");
 grid on;
